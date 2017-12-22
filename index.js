c260c0b925345e553aac66c62d45bf0e49064204
Timothy Kim <ttkim@wesleyan.edu>
2017-12-22 22:31:43 +0900

    why we gotta single when we can double

--- index.js
+++ index.js
@@ -1,82 +1,81 @@
-var express = require('express')
+var express = require("express")
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var redis = require('redis');
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var redis = require("redis");
 
-var client = redis.createClient(process.env.REDIS_URL || 'redis://localhost:6379');
+var client = redis.createClient(process.env.REDIS_URL || "redis://localhost:6379");
 
 // Routing
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/entry', function(req, res){
+app.get("/entry", function(req, res){
   res.sendFile(__dirname + "/public/entry.html")
 });
 
-app.get('/create', function(req, res){
+app.get("/create", function(req, res){
   res.sendFile(__dirname + "/public/create.html")
 });
 
-app.get('/join', function(req, res){
+app.get("/join", function(req, res){
   res.sendFile(__dirname + "/public/join.html")
 });
 
-app.get('/waiting_room', function(req, res){
+app.get("/waiting_room", function(req, res){
   res.sendFile(__dirname + "/public/waiting_room.html")
 });
 
-io.on('connection', function(socket){
+io.on("connection", function(socket){
 
-  socket.on('connect', function() {
-    console.log('A client connected')
+  socket.on("connect", function() {
+    console.log("A client connected")
   })
   
   // Create a channel for a given session
-  socket.on('createChannel', function(userID) {
+  socket.on("createChannel", function(userID) {
 
     // Create a random 6-character string
     const channelID = Math.random().toString(36).substr(2, 5)
 
     // Pass the string to client
-    socket.emit('passChannelID', { id : channelID });
+    socket.emit("passChannelID", { id : channelID });
 
     // Create a channel using the 6-character string
     socket.join(channelID)
 
     // Add the channelID to the list of all active channelID
-    client.sadd('activeChannels', channelID)
+    client.sadd("activeChannels", channelID)
 
-    // Add the user to the channel's list of players
-    client.lpush(channelID + 'players', userID)
+    // Add the user to the channel"s list of players
+    client.lpush(channelID + "players", userID)
 
     console.log("Channel '" + channelID + "' was created by user '" + userID + "'")
 
     // TODO: in more detail
     // initialize the deck of cards by randomly permuting a set and storing it
     // in a named Redis queue (also named the same way as the socket channel)
-    // const shuffledDeck = ['Ac', 'Ad', 'Ah', 'As']
+    // const shuffledDeck = ["Ac", "Ad", "Ah", "As"]
 
-    // var channelOfCards = channelID + 'cards'
+    // var channelOfCards = channelID + "cards"
     // client.lpush(channelOfCards, shuffledDeck)
   })
 
   // Check if a channel is available to be joined
-  socket.on('checkChannel', function(channelID) {
+  socket.on("checkChannel", function(channelID) {
 
     // Grab all channels and check
-    client.smembers('activeChannels', function(err, reply) {
-      var check = (reply.indexOf(channelID) != -1);
+    client.smembers("activeChannels", function(err, reply) {
 
       // Send results
-      socket.emit('validChannel', check);
+      socket.emit("validChannel", (reply.indexOf(channelID) != -1));
     });
   })
 
   // end-point for other players to join a channel
   // params
   // data - JSON {userID, channelID}
-  socket.on('joinChannel', function(data) {
-    console.log(data.userID + ' joined channel ' + data.channelID)
+  socket.on("joinChannel", function(data) {
+    console.log(data.userID + " joined channel " + data.channelID)
 
     // Join a channel
     socket.join(data.channelID)
@@ -84,8 +83,12 @@ io.on('connection', function(socket){
     // Add to list of players
     client.lpush(data.channelID + "players", data.userID)
 
-    // broadcast to all connected players who joined the game
-    socket.emit(data.channelID, data.userID + 'has joined the game')
+    // Tell the host that you joined so it increments
+    // TODO: Use redis incr and decr for player number instead of length
+    socket.emit("joinChannel" + data.channelID)
+
+    // Broadcast to all connected players who joined the game
+    socket.emit(data.channelID + "players", data.userID + "has joined the game")
   })
 
   // when any player does a card uncover, do a Redis pop to select a card to play
@@ -93,10 +96,10 @@ io.on('connection', function(socket){
 
   // params
   // data - JSON {channelID, userID}
-  socket.on('cardPlay', function(data) {
+  socket.on("cardPlay", function(data) {
 
     // do a redis pop to get a card
-    const channelOfCards = data.channelID + 'cards'
+    const channelOfCards = data.channelID + "cards"
     client.lpop(channelOfCards, function(err, card) {
       
       // and get the next player 
@@ -121,8 +124,8 @@ io.on('connection', function(socket){
 
 
 
-  socket.on('disconnect', function () {
-    console.log('A client disconnected');
+  socket.on("disconnect", function () {
+    console.log("A client disconnected");
   });
 
 });
@@ -130,5 +133,5 @@ io.on('connection', function(socket){
 
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
